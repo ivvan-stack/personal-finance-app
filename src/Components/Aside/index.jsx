77915f5fda420377a54de36ce5b320cbb16f9cae
Iván Yllescas Lamas <ivan.yllescas@vicom.mx>
2021-02-09 10:51:06 -0600

    Components & Views  [minor adjustments]

--- src/Components/Aside/index.jsx
+++ src/Components/Aside/index.jsx
@@ -9,9 +9,9 @@ class Aside extends Component {
     return (
       <nav className="panel is-pulled-left col-2_md-12_sm-12 asside-center">
         <div className="notification is-radiusless is-dark">
-          <p className="title is-size-3-fullhd is-size-4-widescreen is-size-5-desktop is-size-2-mobile">Has realizado:</p>
-          <p className="title is-size-3-fullhd is-size-4-widescreen is-size-5-desktop is-size-2-mobile">
-            <span className="title outlined is-size-1 number-of-intents">{this.props.attemps}</span> intentos
+          <p className="title outlined is-size-3-fullhd is-size-4-widescreen is-size-5-desktop is-size-2-mobile">Has realizado:</p>
+          <p className="title outlined is-size-3-fullhd is-size-4-widescreen is-size-5-desktop is-size-2-mobile">
+            <span className="title is-size-1 number-of-intents">{this.props.attemps}</span> intentos
           </p>
           <div className="panel-block">
             <button id="reset-game" className="button button-lineal large white" onClick={this.props.resetGame}>
